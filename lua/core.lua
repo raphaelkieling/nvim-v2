a37a0cae520622aa88bddc2102c5e8f8b7f2b4c8
raphael.kieling <raphael.kieling-ext@ab-inbev.com>
2024-12-20 17:54:05 -0300

    feat: plugins and core

--- lua/core.lua
+++ lua/core.lua
@@ -2,6 +2,8 @@ print("Core loaded")
 
 local opt = vim.opt
 local g = vim.g
+local api = vim.api
+local o = vim.o
 
 g.mapleader = " "
 
@@ -13,24 +15,28 @@ opt.shiftwidth=4
 opt.tabstop=4
 opt.wrap=false
 opt.signcolumn = 'yes'
+o.incsearch = true
+o.ignorecase = true
+o.smartcase = true
+o.history = 1000
+o.scrolloff = 8
 
 -- Disable default tree
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
 
 -- Split Layout
-vim.api.nvim_set_keymap('n', '<C-k>', ':wincmd k<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-j>', ':wincmd j<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-h>', ':wincmd h<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-l>', ':wincmd l<CR>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<C-k>', ':wincmd k<CR>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<C-j>', ':wincmd j<CR>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<C-h>', ':wincmd h<CR>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<C-l>', ':wincmd l<CR>', { noremap = true, silent = true })
 
 -- Terminal 
-vim.api.nvim_set_keymap('t', '<C-e>', [[<C-\><C-n>]], { noremap = true, silent = true })
+api.nvim_set_keymap('t', '<C-e>', [[<C-\><C-n>]], { noremap = true, silent = true })
 
 
 -- Yank highlight
-vim.api.nvim_exec([[
+api.nvim_exec([[
   augroup YankHighlight
     autocmd!
     autocmd TextYankPost * silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=200})
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -23,6 +23,14 @@ return {
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			defaults = {
+				layout_strategy = "horizontal",
+				layout_config = { prompt_position = "top" },
+				sorting_strategy = "ascending",
+				winblend = 0,
+			},
+		},
 		config = function()
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Telescope find files" })
